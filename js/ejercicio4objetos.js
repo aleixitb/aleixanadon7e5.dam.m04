cb665ba5c1777edd5b7774606db5094d3a0802d3
aleix <aleix.anadon.735@itb.cat>
2022-04-05 22:38:32 +0200

    no està arreglat encara, però ho estarà perquè he fet proves

--- js/ejercicio4objetos.js
+++ js/ejercicio4objetos.js
@@ -1,46 +1,42 @@
 // ELEMENTO BOTÓN
-var buttonCreateObject = document.getElementById('createObject');
 var buttonDisplayObject = document.getElementById('showObject');
 
 // AÑADIR EVENTOS AL BOTÓN
-buttonCreateObject.addEventListener("click",createObject );
 buttonDisplayObject.addEventListener("click", displayObject);
 
-// puedo crear una función que cuando pulsas el botón llama a otra función que crea el objeto, y en la función principal guardamos el objeto en una variable.
-// el problema es que eso llama de nuevo a createObject así que habría que buscar otra solución...
-
-// FUNCIÓN QUE CREA EL OBJETO AL PULSAR EL BOTÓN
-function storeObject() {
+// CREA EL OBJETO
+function createObject() {
 
     let name = document.getElementById('name').value;
     let grade = document.getElementById('grade').value;
     let age = document.getElementById('age').value;
 
-    var student = {
+    let student = {
         name: name,
         grade: grade,
         age: age
     }
 
-    console.log("objeto creado");
-    console.log(student);
-
     return student;
 }
 
 // MOSTRA L'OBJECTE CREAT
-function displayObject(student) {
+function displayObject() {
 
-    var student = createObject();
+    let student = createObject();
 
-    var content = `Student {<br>&emsp;name: ${student.name},<br>&emsp;grade: ${student.grade},<br>&emsp;age: ${student.age}<br>}`;
+    let content = `Student {<br>&emsp;name: ${student.name},<br>&emsp;grade: ${student.grade},<br>&emsp;age: ${student.age}<br>}`;
 
-    var element = document.getElementById('student')
+    let element = document.getElementById('student')
 
     if (element.innerHTML == "") {
 
         element.innerHTML = content;
         buttonDisplayObject.innerHTML = "Ocultar objecte";
+
+        for (i in student) {
+            delete student[i];
+        }
 
     } else if (element.innerHTML != "") {
 
@@ -54,7 +50,17 @@ var deleteName = document.getElementById('deleteName');
 var deleteGrade = document.getElementById('deleteGrade');
 var deleteAge = document.getElementById('deleteAge');
 
-function displayButtons() {
-    console.log("student here" + student.age);
-    deleteName.innerHTML = `${student.name}`;
+var objToDelete = document.getElementById('objToDelete');
+
+window.addEventListener("load", showStudent());
+
+function showStudent() {
+    let student = createObject()
+    objToDelete.innerHTML = `Student {<br>&emsp;name: ${student.name},<br>&emsp;grade: ${student.grade},<br>&emsp;age: ${student.age}<br>}`;
 }
+
+deleteName.addEventListener("click", function(){
+
+    delete student.name;
+    showStudent();
+});
