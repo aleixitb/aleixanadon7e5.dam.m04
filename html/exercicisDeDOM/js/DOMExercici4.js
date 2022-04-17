a09b97fc2d1a4ebe3418295e1570040b44c7ca2e
aleix <aleix.anadon.735@itb.cat>
2022-04-17 02:43:26 +0200

    me falta imprimir cada atributo por la consola

---
+++ html/exercicisDeDOM/js/DOMExercici4.js
@@ -0,0 +1,36 @@
+
+var buttonGetAttributes = document.getElementById("buttonGetAttributes");
+
+var linkElement = document.getElementById("itb");
+
+var elementAttributes = linkElement.getAttributeNames();
+
+buttonGetAttributes.addEventListener("click", toggle);
+
+function toggle() {
+
+    let button = buttonGetAttributes;
+
+    if (button.innerHTML == "Clica aqui per obtenir els atributs") {
+
+        obtenirAtributs();
+        button.innerHTML = "Netejar la consola";
+
+    } else if (button.innerHTML == "Netejar la consola") {
+
+        clearConsole();
+        button.innerHTML = "Clica aqui per obtenir els atributs";
+    }
+}
+
+function obtenirAtributs() {
+
+    Array.from(elementAttributes).forEach(element => {
+        console.log(element.indexOf)
+    })
+}
+
+function clearConsole() {
+
+    console.clear();
+}
