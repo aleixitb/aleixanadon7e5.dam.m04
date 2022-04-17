d39ba23bc14e4364bb2dbffa02439871dc7f62b5
aleix <aleix.anadon.735@itb.cat>
2022-04-17 17:09:39 +0200

    ja he fet que funcioni lo de les taules

---
+++ html/exercicisDeDOM/js/DOMExercici5.js
@@ -0,0 +1,73 @@
+var table = document.getElementById("table");
+
+var buttonBeggining = document.getElementById("buttonBeggining");
+var buttonEnd = document.getElementById("buttonEnd");
+
+buttonBeggining.addEventListener("click", toggleBeggining);
+buttonEnd.addEventListener("click", toggleEnd);
+
+function toggleBeggining() {
+
+    let button = buttonBeggining;
+    
+    if (button.innerHTML == "Inserta fila al principi") {
+
+        insertCellBeggining();
+        button.innerHTML = "Esborrar fila"
+
+    } else if (button.innerHTML == "Esborrar fila") {
+
+        deleteCellBeggining();
+        button.innerHTML = "Inserta fila al principi"
+    }
+}
+
+function insertCellBeggining() {
+
+    let row = table.insertRow(0);
+
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Fila 0 cel·la 1";
+    cell2.innerHTML = "Fila 0 cel·la 2";
+}
+
+function deleteCellBeggining() {
+
+    let row = document.querySelector("#table:first-child");
+    table.deleteRow(0);
+}
+
+function toggleEnd() {
+
+    let button = buttonEnd;
+
+    if (button.innerHTML == "Insertar fila al final") {
+
+        insertCellEnd();
+        button.innerHTML = "Esborrar fila";
+
+    } else if (button.innerHTML == "Esborrar fila") {
+
+        deleteCellEnd();
+        button.innerHTML = "Insertar fila al final"
+    }
+}
+
+function insertCellEnd() {
+
+    let row = table.insertRow(-1);
+
+    let cell1 = row.insertCell(-1);
+    let cell2 = row.insertCell(-1);
+
+    cell1.innerHTML = "Fila 3 cel·la 1";
+    cell2.innerHTML = "Fila 3 cel·la 2";
+}
+
+function deleteCellEnd() {
+
+    let row = document.querySelector("#table:last-child");
+    table.deleteRow(-1);
+}
