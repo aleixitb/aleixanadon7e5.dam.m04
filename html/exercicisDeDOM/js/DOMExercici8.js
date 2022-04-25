8c54bf8f0257f53b366839024e1ee940b3995434
aleix <aleix.anadon.735@itb.cat>
2022-04-25 19:02:12 +0200

    ja funciona el 8

---
+++ html/exercicisDeDOM/js/DOMExercici8.js
@@ -0,0 +1,28 @@
+var deleteFromListButton = document.getElementById("deleteFromList");
+var select = document.querySelector("select");
+var log = document.getElementById("log");
+
+function getOption() {
+
+        let opt;
+
+        for ( let i = 0, len = select.options.length; i < len; i++ ) {
+            opt = select.options[i];
+            if ( opt.selected === true ) {
+                break;
+            }
+        }
+
+        return opt;
+}
+
+function deleteOption() {
+
+    let opt = getOption();
+
+    select.remove(opt.index);
+    
+    log.innerHTML = ` ${opt.value} deleted`;
+}
+
+deleteFromListButton.addEventListener("click", deleteOption);
