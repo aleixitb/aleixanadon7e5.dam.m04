a13b0a704cc95b6bab0b9e4484b8ddff70e1e76a
aleixitb <aleix.anadon.7e5@itb.cat>
2022-05-06 10:14:02 +0200

    ja puc comparar els dos codis, ara només fa falta limitar el nº d'intents, canviar el color dels divs i canviar els missatges que dona el joc i fer testing per omplir els forats del codi

--- html/ejercicio2codigosecreto/script.js
+++ html/ejercicio2codigosecreto/script.js
@@ -1,12 +1,15 @@
 const secretCode = [];
 const maxIntento = 8;
+const codeLength = 5
 
 function generateCode() {
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < codeLength; i++) {
 
         secretCode[i] = Math.floor((Math.random() * 10));
     }
+
+    console.log(`secretCode: ${secretCode}`);
 
     return secretCode;
 }
@@ -18,44 +21,66 @@ const buttonCheck = document.getElementById('check');
 function getCodeFromPage() {
 
     let webCode = document.getElementById('numero').value;
-    console.log(webCode);
+    let webCodeToArray = [];
+
+    for (let i = 0; i <= webCode.length; i++) {
+
+        webCodeToArray.push(webCode[i])
+    }
+
+    return webCodeToArray;
 }
 
-buttonCheck.addEventListener("click", getCodeFromPage);
+buttonCheck.addEventListener("click", play);
 
 var section = document.getElementById("result");
 
-function generateDivs() {
+function generateDivs(webCodeToArray) {
 
-    // ahora hay que extrapolarlo a un bucle
+    let grandpaDiv = document.createElement("div");
+    grandpaDiv.setAttribute("class", "rowResult w100 flex wrap");
+
+    for (let i = 0; i < codeLength; i++) {
+
+        let parentDiv = document.createElement("div"); 
+        parentDiv.setAttribute("class", "w20");
+
+        let childDiv = document.createElement("div");
+        childDiv.setAttribute("class", "celResult flex");
+    
+        childDiv.innerHTML = webCodeToArray[i];
+
+        grandpaDiv.appendChild(parentDiv);
+        parentDiv.appendChild(childDiv);
+    }
+
+    section.appendChild(grandpaDiv);
 }
 
-generateDivs();
+function check(webCode) {
+
+    for (let i = 0; i < secretCode.length; i++) {
+
+        for (let j = 0; j < webCode.length; j++) {
+
+            if (secretCode[i] == webCode[j] && i == j) {
+
+                console.log("rightPosition: " + webCode[j]);
+            
+            } else if (secretCode[i] == webCode[j] && i != j) {
+
+                console.log("badPosition: " + webCode[j]);
+            }
+        }
+    }
+}
 
 // Aquí se itera con la constante maxIntento
 
 function play() {
 
+    let webCode = getCodeFromPage();
+    generateDivs(webCode);
+    console.log(`webCode: ${webCode}`);
+    check(webCode);
 }
-
-// pruebas para construir las tablas de divs
-
-let grandpaDiv = document.createElement("div");
-grandpaDiv.setAttribute("class", "rowResult w100 flex wrap");
-
-console.log("grandpaDiv: " + grandpaDiv);
-
-let parentDiv = document.createElement("div"); 
-parentDiv.setAttribute("class", "w20");
-
-console.log("parentDiv: " + parentDiv);
-
-let childDiv = document.createElement("div");
-childDiv.setAttribute("class", "celResult flex");
-
-console.log("childDiv: " + childDiv);
-
-grandpaDiv.appendChild(parentDiv);
-parentDiv.appendChild(childDiv);
-
-section.appendChild(grandpaDiv);
