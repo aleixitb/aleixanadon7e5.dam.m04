3cecbbc2d782005947fab1d454218ff0c1da6d4b
aleix <aleix.anadon.735@itb.cat>
2022-05-07 22:47:16 +0200

    m'he apuntat lo de la consola per demà

--- html/ejercicio2codigosecreto/script.js
+++ html/ejercicio2codigosecreto/script.js
@@ -110,3 +110,63 @@ function play() {
     let checkCode = check(webCode);
     generateDivs(webCode, checkCode);
 }
+
+/*
+var secret = [7,6,5,7,3];
+var guess =  [1,2,3,6,7];
+var list = [];
+
+for (i in secret) {
+    
+    if (secret.includes(guess[i])) {
+
+        console.log("i: " + guess[i])
+        
+        for (j in guess) {
+        
+            if (secret[i] == guess[j]) {
+            
+                if (i == j) {
+
+                    list.push(
+                        {
+                        position: "rightPosition",
+                        number: guess[j]
+                        }
+                    )
+                } else {
+
+                    list.push(
+                        {
+                        position: "badPosition",
+                        number: guess[j]
+                        }
+                        )
+                }
+            }
+        }
+               
+    } else if (!(secret.includes(guess[i]))) {
+        list.push(
+            {
+            position: "wrong", 
+            number: guess[i]
+            }
+        )
+    }
+}
+    
+
+
+console.log(secret);
+console.log(guess);
+console.log(list);
+
+VM3956:9 i: 3
+VM3956:9 i: 6
+VM3956:9 i: 7
+VM3956:47 (5) [7, 6, 5, 7, 3]
+VM3956:48 (5) [1, 2, 3, 6, 7]
+VM3956:49 (4) [{…}, {…}, {…}, {…}]0: {position: 'wrong', number: 1}1: {position: 'wrong', number: 2}2: {position: 'badPosition', number: 7}3: {position: 'badPosition', number: 3}length: 4[[Prototype]]: Array(0)
+undefined
+*/
