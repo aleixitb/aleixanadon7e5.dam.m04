f1bde97a8cc213b43e42f0eb7ba3d42e825de2e0
sjo <aleix.anadon.7e5@itb.cat>
2022-05-09 11:59:52 +0200

    funciona el css, em falta el maxIntents

--- html/ejercicio2codigosecreto/script.js
+++ html/ejercicio2codigosecreto/script.js
@@ -1,4 +1,4 @@
-const secretCode = [];
+const secret = [];
 const maxIntento = 8;
 const codeLength = 5
 
@@ -6,43 +6,43 @@ function generateCode() {
 
     for (let i = 0; i < codeLength; i++) {
 
-        secretCode[i] = Math.floor((Math.random() * 10));
+        secret[i] = Math.floor((Math.random() * 10));
     }
 
-    console.log(`secretCode: ${secretCode}`);
+    console.log(`secret: ${secret}`);
 
-    return secretCode;
+    return secret;
 }
 
 generateCode();
 
-document.getElementById("secretCode").innerHTML = secretCode;
+document.getElementById("secretCode").innerHTML = secret;
 
 const buttonCheck = document.getElementById('check');
 
 function getCodeFromPage() {
 
     let webCode = document.getElementById('numero').value;
-    let webCodeToArray = [];
+    let guess = [];
 
     for (let i = 0; i <= webCode.length; i++) {
 
-        webCodeToArray.push(webCode[i])
+        guess.push(webCode[i])
     }
 
-    return webCodeToArray;
+    return guess;
 }
 
 buttonCheck.addEventListener("click", play);
 
 var section = document.getElementById("result");
 
-function generateDivs(webCodeToArray, numbers) {
+function generateDivs(guess) {
 
     let grandpaDiv = document.createElement("div");
     grandpaDiv.setAttribute("class", "rowResult w100 flex wrap");
     
-    console.log(numbers)
+    console.log("webCode: " + guess);
 
     for (let i = 0; i < codeLength; i++) {
 
@@ -51,9 +51,20 @@ function generateDivs(webCodeToArray, numbers) {
 
         let childDiv = document.createElement("div");
 
-        childDiv.setAttribute("class", `celResult flex ${numbers[i].position}`);
-    
-        childDiv.innerHTML = webCodeToArray[i];
+        if (guess[i] == secret[i]) { 
+            
+            childDiv.setAttribute("class", `celResult flex rightPosition`);
+
+        } else if ((secret.includes(parseInt(guess[i]))) && (guess[i] != secret[i])) {
+
+            childDiv.setAttribute("class", `celResult flex badPosition`);
+
+        } else {
+
+            childDiv.setAttribute("class", `celResult flex wrong`);
+        }
+
+        childDiv.innerHTML = guess[i];
 
         grandpaDiv.appendChild(parentDiv);
         parentDiv.appendChild(childDiv);
@@ -62,111 +73,10 @@ function generateDivs(webCodeToArray, numbers) {
     section.appendChild(grandpaDiv);
 }
 
-function check(webCode) {
-
-    let list = [];
-
-    secretCode.forEach((i) => {
-        
-        webCode.forEach((j) => {
-
-            if (secretCode[i] == webCode[j] && i == j) {
-                list.push(
-                    {
-                    position: "rightPosition",
-                    number: webCode[j]
-                    }
-                )
-
-            } else if (secretCode[i] == webCode[j] && i != j) {
-
-                list.push(
-                    {
-                    position: "badPosition",
-                    number: webCode[j]
-                    }
-                )
-
-            } else if (!(webCode.includes(i))) {
-
-                list.push(
-                    {
-                    position: "wrong", 
-                    number: webCode[j]
-                    }
-                )
-            }
-        })
-    })
-
-    return list
-}
-
 // Aquí se itera con la constante maxIntento
 
 function play() {
 
     let webCode = getCodeFromPage();
-    let checkCode = check(webCode);
-    generateDivs(webCode, checkCode);
+    generateDivs(webCode);
 }
-
-/*
-var secret = [7,6,5,7,3];
-var guess =  [1,2,3,6,7];
-var list = [];
-
-for (i in secret) {
-    
-    if (secret.includes(guess[i])) {
-
-        console.log("i: " + guess[i])
-        
-        for (j in guess) {
-        
-            if (secret[i] == guess[j]) {
-            
-                if (i == j) {
-
-                    list.push(
-                        {
-                        position: "rightPosition",
-                        number: guess[j]
-                        }
-                    )
-                } else {
-
-                    list.push(
-                        {
-                        position: "badPosition",
-                        number: guess[j]
-                        }
-                        )
-                }
-            }
-        }
-               
-    } else if (!(secret.includes(guess[i]))) {
-        list.push(
-            {
-            position: "wrong", 
-            number: guess[i]
-            }
-        )
-    }
-}
-    
-
-
-console.log(secret);
-console.log(guess);
-console.log(list);
-
-VM3956:9 i: 3
-VM3956:9 i: 6
-VM3956:9 i: 7
-VM3956:47 (5) [7, 6, 5, 7, 3]
-VM3956:48 (5) [1, 2, 3, 6, 7]
-VM3956:49 (4) [{…}, {…}, {…}, {…}]0: {position: 'wrong', number: 1}1: {position: 'wrong', number: 2}2: {position: 'badPosition', number: 7}3: {position: 'badPosition', number: 3}length: 4[[Prototype]]: Array(0)
-undefined
-*/
